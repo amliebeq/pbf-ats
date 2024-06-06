f1aa939d515ebeadcee0900ac0c6c98798a985ca
amliebeq <alex.lieberman182@gmail.com>
2024-06-06 13:42:03 -0700

    created jobs routes

--- backend/routes/applicants.js
+++ backend/routes/applicants.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 require('dotenv').config();
 const supabase = require('../config/db');
+const authMiddleware = require('../config/auth');
 
-router.get('/', async (req, res) => {
+router.get('/', authMiddleware, async (req, res) => {
     try {
         const { data, error } = await supabase
             .from('applicants')
@@ -17,7 +18,77 @@ router.get('/', async (req, res) => {
     } catch (error) {
         console.log(error);
         res.status(500).send(error);
-    }  
+    };
+});
+
+router.post('/', authMiddleware, async (req, res) => {
+    const { user_id, first_name, last_name, phone_number, recent_job, email, job_id, list_id } = req.body;
+    try{
+        const { data, error } = await supabase
+            .from('applicants')
+            .insert({ user_id, first_name, last_name, phone_number, recent_job, email, job_id, list_id })
+            .select();
+        if (error) {
+            console.log(error);
+            res.status(500).send(error);
+        } else {
+            res.send(data);
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    };
+});
+
+router.delete('/:id', authMiddleware, async (req, res) => {
+    const { id } = req.params;
+    try{
+        const { data: deletedApplicant, error: deleteError } = await supabase
+            .from('applicants')
+            .delete()
+            .eq('id', id)
+            .select();
+        
+        if (deleteError) {
+            console.error(deleteError);
+            return res.status(500).json({ error: deleteError.message });
+        }
+        
+        if (!deletedApplicant || deletedApplicant.length === 0) {
+            return res.status(404).json({ error: 'Applicant not found' });
+        }
+        
+        return res.status(200).json({ message: `Applicant deleted successfully` });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: error.message });
+    };
+});
+
+router.put('/:id', authMiddleware, async (req, res) => {
+    const { id } = req.params;
+    const { first_name, last_name, phone_number, recent_job, email, job_id, list_id } = req.body;
+    try{
+        const { data: updatedApplicant, error: updateError } = await supabase
+            .from('applicants')
+            .update({ first_name, last_name, phone_number, recent_job, email, job_id, list_id })
+            .eq('id', id)
+            .select();
+        
+        if (updateError) {
+            console.error(updateError);
+            return res.status(500).json({ error: updateError.message });
+        }
+        
+        if (!updatedApplicant || updatedApplicant.length === 0) {
+            return res.status(404).json({ error: 'Applicant not found' });
+        }
+        
+        return res.status(200).json({ updatedApplicant });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: error.message });
+    };
 });
 
 module.exports = router;
--- backend/routes/jobs.js
+++ backend/routes/jobs.js
@@ -1,9 +1,95 @@
 const express = require('express');
 const router = express.Router();
+require('dotenv').config();
+const supabase = require('../config/db');
+const authMiddleware = require('../config/auth');
 
-// Define your routes here
-router.get('/', (req, res) => {
-  res.send('Jobs route');
+router.get('/', authMiddleware, async (req, res) => {
+    try {
+        const { data, error } = await supabase
+            .from('jobs')
+            .select();
+        if (error) {
+            console.log(error);
+            res.status(500).send(error);
+        } else {
+            res.send(data);
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    };
+});
+
+router.post('/', authMiddleware, async (req, res) => {
+    const { user_id, title, pay_rate, description, location, remote } = req.body;
+    try{
+        const { data, error } = await supabase
+            .from('jobs')
+            .insert({ user_id, title, pay_rate, description, location, remote })
+            .select();
+        if (error) {
+            console.log(error);
+            res.status(500).send(error);
+        } else {
+            res.send(data);
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    };
+});
+
+router.delete('/:id', authMiddleware, async (req, res) => {
+    const { id } = req.params;
+    try{
+        const { data: deletedJob, error: deleteError } = await supabase
+            .from('jobs')
+            .delete()
+            .eq('id', id)
+            .select();
+        
+        if (deleteError) {
+            console.error(deleteError);
+            return res.status(500).json({ error: deleteError.message });
+        }
+        
+        if (!deletedJob || deletedJob.length === 0) {
+            return res.status(404).json({ error: 'Job not found' });
+        }
+        
+        return res.status(200).json({ message: `Job deleted successfully` });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: error.message });
+    };
+});
+
+router.put('/:id', authMiddleware, async (req, res) => {
+    const { id } = req.params;
+    const { title, pay_rate, description, location, remote } = req.body;
+    try{
+        const { data: updatedJob, error: updateError } = await supabase
+            .from('jobs')
+            .update({ title, pay_rate, description, location, remote })
+            .eq('id', id)
+            .select();
+        
+        if (updateError) {
+            console.error(updateError);
+            return res.status(500).json({ error: updateError.message });
+        }
+        
+        if (!updatedJob || updatedJob.length === 0) {
+            return res.status(404).json({ error: 'Job not found' });
+        }
+        
+        return res.status(200).json({ updatedJob });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: error.message });
+    };
 });
 
 module.exports = router;
+
